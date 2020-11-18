eb2c32071f65f1f64ba9b396d38045aa8f50eefe
MUNMUN-M <73881222+MUNMUN-M@users.noreply.github.com>
2020-11-18 16:29:06 +0900

    Update erc201.sol

--- back/contracts/erc201.sol
+++ back/contracts/erc201.sol
@@ -3,7 +3,7 @@
 
 pragma solidity ^0.7.0;
 
-import "./context1.sol";
+import "./context.sol";
 import "./safeMath.sol";
 
 /**
@@ -30,7 +30,7 @@ import "./safeMath.sol";
  * functions have been added to mitigate the well-known issues around setting
  * allowances. See {IERC20-approve}.
  */
-contract ERC201 is Context1 {
+contract ERC201 is Context {
     using SafeMath for uint256;
 
     mapping (address => mapping (uint8 => uint256)) private _balances1;
@@ -147,83 +147,83 @@ contract ERC201 is Context1 {
      * - the caller must have a balance of at least `amount`.
      */
     function transfer1(address recipient, uint8 _market_id, uint256 amount) public returns (bool) {
-        _transfer1(_msgSender1(), recipient, _market_id, amount);
+        _transfer1(_msgSender(), recipient, _market_id, amount);
         return true;
     }
 
-    /**
-     * @dev See {IERC20-allowance}.
-     */
-    function allowance1(address owner, address spender, uint8 _market_id) public view returns (uint256) {
-        return _allowances1[owner][spender][_market_id];
-    }
+    // /**
+    //  * @dev See {IERC20-allowance}.
+    //  */
+    // function allowance1(address owner, address spender, uint8 _market_id) public view returns (uint256) {
+    //     return _allowances1[owner][spender][_market_id];
+    // }
 
-    /**
-     * @dev See {IERC20-approve}.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     */
-    function approve1(address spender, uint256 amount, uint8 _market_id) public returns (bool) {
-        _approve1(_msgSender1(), spender, _market_id, amount);
-        return true;
-    }
+    // /**
+    //  * @dev See {IERC20-approve}.
+    //  *
+    //  * Requirements:
+    //  *
+    //  * - `spender` cannot be the zero address.
+    //  */
+    // function approve1(address spender, uint256 amount, uint8 _market_id) public returns (bool) {
+    //     _approve1(_msgSender1(), spender, _market_id, amount);
+    //     return true;
+    // }
 
-    /**
-     * @dev See {IERC20-transferFrom}.
-     *
-     * Emits an {Approval} event indicating the updated allowance. This is not
-     * required by the EIP. See the note at the beginning of {ERC20}.
-     *
-     * Requirements:
-     *
-     * - `sender` and `recipient` cannot be the zero address.
-     * - `sender` must have a balance of at least `amount`.
-     * - the caller must have allowance for ``sender``'s tokens of at least
-     * `amount`.
-     */
-    function transferFrom1(address sender, address recipient, uint8 _market_id, uint256 amount) public returns (bool) {
-        _transfer1(sender, recipient, _market_id, amount);
-        _approve1(sender, _msgSender1(), _market_id, _allowances1[sender][_msgSender1()][_market_id].sub(amount, "ERC20: transfer amount exceeds allowance"));
-        return true;
-    }
+    // /**
+    //  * @dev See {IERC20-transferFrom}.
+    //  *
+    //  * Emits an {Approval} event indicating the updated allowance. This is not
+    //  * required by the EIP. See the note at the beginning of {ERC20}.
+    //  *
+    //  * Requirements:
+    //  *
+    //  * - `sender` and `recipient` cannot be the zero address.
+    //  * - `sender` must have a balance of at least `amount`.
+    //  * - the caller must have allowance for ``sender``'s tokens of at least
+    //  * `amount`.
+    //  */
+    // function transferFrom1(address sender, address recipient, uint8 _market_id, uint256 amount) public returns (bool) {
+    //     _transfer1(sender, recipient, _market_id, amount);
+    //     _approve1(sender, _msgSender1(), _market_id, _allowances1[sender][_msgSender1()][_market_id].sub(amount, "ERC20: transfer amount exceeds allowance"));
+    //     return true;
+    // }
 
-    /**
-     * @dev Atomically increases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     */
-    function increaseAllowance1(address spender, uint8 _market_id, uint256 addedValue) public returns (bool) {
-        _approve1(_msgSender1(), spender, _market_id, _allowances1[_msgSender1()][spender][_market_id].add(addedValue));
-        return true;
-    }
+    // /**
+    //  * @dev Atomically increases the allowance granted to `spender` by the caller.
+    //  *
+    //  * This is an alternative to {approve} that can be used as a mitigation for
+    //  * problems described in {IERC20-approve}.
+    //  *
+    //  * Emits an {Approval} event indicating the updated allowance.
+    //  *
+    //  * Requirements:
+    //  *
+    //  * - `spender` cannot be the zero address.
+    //  */
+    // function increaseAllowance1(address spender, uint8 _market_id, uint256 addedValue) public returns (bool) {
+    //     _approve1(_msgSender1(), spender, _market_id, _allowances1[_msgSender1()][spender][_market_id].add(addedValue));
+    //     return true;
+    // }
 
-    /**
-     * @dev Atomically decreases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     * - `spender` must have allowance for the caller of at least
-     * `subtractedValue`.
-     */
-    function decreaseAllowance1(address spender, uint8 _market_id, uint256 subtractedValue) public returns (bool) {
-        _approve1(_msgSender1(), spender, _market_id, _allowances1[_msgSender1()][spender][_market_id].sub(subtractedValue, "ERC20: decreased allowance below zero"));
-        return true;
-    }
+    // /**
+    //  * @dev Atomically decreases the allowance granted to `spender` by the caller.
+    //  *
+    //  * This is an alternative to {approve} that can be used as a mitigation for
+    //  * problems described in {IERC20-approve}.
+    //  *
+    //  * Emits an {Approval} event indicating the updated allowance.
+    //  *
+    //  * Requirements:
+    //  *
+    //  * - `spender` cannot be the zero address.
+    //  * - `spender` must have allowance for the caller of at least
+    //  * `subtractedValue`.
+    //  */
+    // function decreaseAllowance1(address spender, uint8 _market_id, uint256 subtractedValue) public returns (bool) {
+    //     _approve1(_msgSender1(), spender, _market_id, _allowances1[_msgSender1()][spender][_market_id].sub(subtractedValue, "ERC20: decreased allowance below zero"));
+    //     return true;
+    // }
 
     /**
      * @dev Moves tokens `amount` from `sender` to `recipient`.
@@ -303,40 +303,40 @@ contract ERC201 is Context1 {
      * - `owner` cannot be the zero address.
      * - `spender` cannot be the zero address.
      */
-    function _approve1(address owner, address spender, uint8 _market_id, uint256 amount) internal {
-        require(owner != address(0), "ERC20: approve from the zero address");
-        require(spender != address(0), "ERC20: approve to the zero address");
+    // function _approve1(address owner, address spender, uint8 _market_id, uint256 amount) internal {
+    //     require(owner != address(0), "ERC20: approve from the zero address");
+    //     require(spender != address(0), "ERC20: approve to the zero address");
 
-        _allowances1[owner][spender][_market_id] = amount;
-        emit Approval1(owner, spender, _market_id, amount);
-    }
+    //     _allowances1[owner][spender][_market_id] = amount;
+    //     emit Approval1(owner, spender, _market_id, amount);
+    // }
 
-    /**
-     * @dev Sets {decimals} to a value other than the default one of 18.
-     *
-     * WARNING: This function should only be called from the constructor. Most
-     * applications that interact with token contracts will not expect
-     * {decimals} to ever change, and may work incorrectly if it does.
-     */
-    function _setupDecimals1(uint8 decimals_) internal {
-        _decimals1 = decimals_;
-    }
+    // /**
+    //  * @dev Sets {decimals} to a value other than the default one of 18.
+    //  *
+    //  * WARNING: This function should only be called from the constructor. Most
+    //  * applications that interact with token contracts will not expect
+    //  * {decimals} to ever change, and may work incorrectly if it does.
+    //  */
+    // function _setupDecimals1(uint8 decimals_) internal {
+    //     _decimals1 = decimals_;
+    // }
 
-    /**
-     * @dev Hook that is called before any transfer of tokens. This includes
-     * minting and burning.
-     *
-     * Calling conditions:
-     *
-     * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
-     * will be to transferred to `to`.
-     * - when `from` is zero, `amount` tokens will be minted for `to`.
-     * - when `to` is zero, `amount` of ``from``'s tokens will be burned.
-     * - `from` and `to` are never both zero.
-     *
-     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
-     */
-    // function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual { }
+    // /**
+    //  * @dev Hook that is called before any transfer of tokens. This includes
+    //  * minting and burning.
+    //  *
+    //  * Calling conditions:
+    //  *
+    //  * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
+    //  * will be to transferred to `to`.
+    //  * - when `from` is zero, `amount` tokens will be minted for `to`.
+    //  * - when `to` is zero, `amount` of ``from``'s tokens will be burned.
+    //  * - `from` and `to` are never both zero.
+    //  *
+    //  * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+    //  */
+    // // function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual { }
 
      //market_ids 안에 이미 존재하는지 체크;
     function _already_market_id1(uint8 _market_id) internal view returns (bool) {
